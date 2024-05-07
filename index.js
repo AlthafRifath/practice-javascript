61d714cf80ac4b6960beed6f14e8e73d3a18aeed
Althaf <althafrifath@gmail.com>
2024-05-07 12:11:20 +0530

    Checked Property

--- index.js
+++ index.js
@@ -1,56 +1,33 @@
-// IF STATEMENTS = if a condition is true, execute some code if not, do something 
+// .checked = property that determines the checked state of an HTML checkbox or radio button element 
 
-let age;
-let hasLicense = true;
-const myText = document.getElementById("myText");
+const myCheckBox = document.getElementById("myCheckBox");
+const visaButton = document.getElementById("visaButton");
+const masterCardButton = document.getElementById("masterCardButton");
+const payPalButton = document.getElementById("payPalButton");
 const mySubmit = document.getElementById("mySubmit");
-const resultElement = document.getElementById("resultElement");
-
-// 1
-if (age >= 18){
-    console.log("You are old enough to drive")
-
-    if(hasLicense){
-        console.log("You have your license!")
-    }else{
-        console.log("You do not have your license yet!");
-    }
-
-}else{
-    console.log("You must be 18+ to have a license")
-}
-
-
-// 2
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
 
 mySubmit.onclick = function() {
 
-    age = myText.value;
-    age = Number(age)
-    if(age >= 120) {
-        resultElement.textContent = `You are too old to enter this site`
-    }
-    else if (age == 0){
-        resultElement.textContent = `You can't enter you were just born`
-    }
-    else if(age >= 18){
-        resultElement.textContent = `You are old enough to enter this site`
-    }
-    else if(age < 0){
-        resultElement.textContent = `Enter a valid age`
+    if(myCheckBox.checked) {
+        subResult.textContent = `You are subscribed`
     }
     else{
-        resultElement.textContent = `You must be 18+ to enter this site`
+        subResult.textContent = `You are not subscribed`
     }
+
+    if(visaButton.checked){
+        paymentResult.textContent = `You are paying with Visa`
+    }
+    else if(masterCardButton.checked){
+        paymentResult.textContent = ` You are paying with MasterCard`
+    }
+    else if(payPalButton.checked){
+        paymentResult.textContent = ` You are paying with PayPal`
+    }
+    else{
+        paymentResult.textContent = `Select a payment a method`
+    }
+
 }
-
-
-// 3
-let isStudent = true;
-
-if (isStudent){
-    console.log("You are a student")
-}else{
-    console.log("You are not a student")
-}
-
