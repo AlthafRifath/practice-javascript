6bb4650ad370802723a64abf95ae9b4222ac14b7
Althaf <althafrifath@gmail.com>
2024-05-07 17:39:35 +0530

    Switch

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-// ternary operator = a shortcut to if{} and else{} statements helps to assign a variable based on a condition 
-//                    condition ? codeIfTrue : CodeIfFalse;
+// SWITCH = can be an efficient replacement to many else if statements 
 
-let age = 21;
-let message = age >= 18 ? "You are an Adult" : "You are not an Adult";
-console.log(message);
+let day = 1;
 
-let time = 10;
-let greeting = time < 12 ? "Good Morning!" : "Good Afternoon!";
-console.log(greeting);
+switch(day){
+    case 1:
+        console.log("It is Monday");
+        break;
+    case 2:
+        console.log("It is Tuesday");
+        break;
+    case 3:
+        console.log("It is Wednesday");
+        break;
+    case 4:
+        console.log("It is Thursday");
+        break;
+    case 5:
+        console.log("It is Friday");
+        break;
+    case 6:
+        console.log("It is Saturday");
+        break;
+    case 7:
+        console.log("It is Saturday");
+        break;
+    default:
+        console.log(`${day} is not a valid day`);
+        break;
+}
 
-let isStudent = true;
-let inform = isStudent ? "Yes" : "No";
-console.log(inform);
+let testScore = 95;
+let letterGrade;
 
-let purchaseAmount = 150;
-let discount = purchaseAmount >= 100 ? 10 : 0;
-console.log(`Your total is $${purchaseAmount - purchaseAmount * (discount/10)}`)
+switch(true){
+    case testScore >= 90:
+        letterGrade = "A";
+        break;
+    default:
+        letterGrade = "F"
+}
+
+console.log(letterGrade)
